e0076385bf9ef97ff7e1707ba3a791a2bfcea9fb
DanilaBratinov <bratinov0704@inbox.ru>
2022-10-29 19:48:08 +0300

    Create ColorApp

--- HW4/ViewController.swift
+++ HW4/ViewController.swift
@@ -1,19 +1,74 @@
-//
-//  ViewController.swift
-//  HW4
-//
-//  Created by Данила Братинов on 29.10.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //MARK: - IB Outlet
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redMeaning: UILabel!
+    @IBOutlet var greenMeaning: UILabel!
+    @IBOutlet var blueMeaning: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMeaning()
+        setupSliders()
+        slidersAction()
     }
-
-
+    
+    //MARK: - IB Action
+    
+    @IBAction func slidersAction() {
+        colorView.backgroundColor = UIColor.init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value), alpha: 1
+        )
+        
+        redMeaning.text = String(format: "%.2f", redSlider.value)
+        greenMeaning.text = String(format: "%.2f", greenSlider.value)
+        blueMeaning.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    
+    
+    //MARK: - Private method
+    override func viewWillLayoutSubviews() {
+        colorView.layer.cornerRadius = colorView.frame.width / 15
+    }
+    
+    private func setupMeaning() {
+        redMeaning.text = "0.00"
+        greenMeaning.text = "0.00"
+        blueMeaning.text = "0.00"
+    }
+    
+    private func setupSliders() {
+        func setupRedSlider() {
+            redSlider.value = 1
+            redSlider.minimumTrackTintColor = .red
+            redSlider.minimumValue = 0
+            redSlider.maximumValue = 1
+        }
+        
+        func setupGreenSlider() {
+            greenSlider.value = 1
+            greenSlider.minimumTrackTintColor = .green
+            greenSlider.minimumValue = 0
+            redSlider.maximumValue = 1
+        }
+        
+        func setupBlueSlider() {
+            blueSlider.value = 1
+            blueSlider.minimumTrackTintColor = .blue
+            blueSlider.minimumValue = 0
+            blueSlider.maximumValue = 1
+        }
+    }
 }
-
